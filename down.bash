e780705e56d02205f35253c58c516065d3d62ca9
Slick AI <mahermah1976@gmail.com>
2026-06-12 20:05:34 +0300

    Set up GitHub Actions and rename container & volume names

--- down.bash
+++ down.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" == "--prod" ]; then
-    sudo COMPOSE_BAKE=true docker compose -f compose.prod.yml down
+    sudo COMPOSE_BAKE=true docker compose -f compose.yml down
     echo 'Stopped production server'
 else
     sudo COMPOSE_BAKE=true docker compose -f compose.dev.yml down
--- up.bash
+++ up.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" == "--prod" ]; then
     echo 'Running production server'
-    sudo COMPOSE_BAKE=true docker compose -f compose.prod.yml up --build -d
+    sudo COMPOSE_BAKE=true docker compose -f compose.yml up --build -d
 else
     echo 'Running development server'
     sudo COMPOSE_BAKE=true docker compose -f compose.dev.yml up --build
